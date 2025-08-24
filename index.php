c39ab281238cc310a2eaf64884fa2cb5e81a2575
msnataala <mnataalasaifullah@gmail.com>
2025-08-24 17:09:34 +0100

    git testing

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'ClassA.php';
+require_once 'ClassB.php';
+
+/*$AInstance = new \App\classA();
+$BInstance = new \App\classB();
+
+echo $AInstance->getName().PHP_EOL;
+echo $BInstance->getName().PHP_EOL;*/
+
+echo \App\ClassA::getName().PHP_EOL;
+echo \App\ClassB::getName().PHP_EOL;
+
+
+
+?>
